102f43e3c6a0f8abd9918d7878f971db03822421
ar-sp <arspksingh@gmail.com>
2025-06-23 14:58:15 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+project = 'HP Color LaserJet Pro MFP M479fdw Printer'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+html_js_files = [
+    'chatbot.js',
+]
